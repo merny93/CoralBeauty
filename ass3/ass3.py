70baa684c34511a28ad69200d1950ae932c29f7c
Simon Tartakovksy <simtart@gmail.com>
2020-10-06 21:50:23 -0400

    deleted local output and made a runner

---
+++ ass3/ass3.py
@@ -0,0 +1,7 @@
+from subprocess import call
+
+call(["python", "p1.py"])
+call(["python", "p2.py"])
+call(["python", "p3.py"])
+call(["python", "p4.py"])
+call(["python", "p5.py"])
--- ass3/p5.py
+++ ass3/p5.py
@@ -4,24 +4,31 @@ import time
 from matplotlib import pyplot as plt
 
 import sys
-sys.stdout = open('output/p4_out.txt', 'w')
+sys.stdout = open('output/p5_out.txt', 'w')
+
+
+
+##alterntivly we can just re-weigh the previous chain:
+old_chain = np.load("output/chain2.npy")
+weights  = np.exp(-(old_chain[:,3] - 0.0544)**2/0.0073**2)
+print("from old chain:")
+print("Best params are now: ", np.sum(old_chain*weights[:,np.newaxis], axis = 0)/np.sum(weights))
+print("with errors given by: ", np.std(old_chain*weights[:,np.newaxis], axis = 0))
 
 def run_mcmc(pars,data,corr_mat, chifun = ct.my_chi, nstep=500, time_out = None):
     start_time = time.time()
-    npar=len(pars)
+    npar=len(pars) ##ignore
     chain=[]
     chivec=[]
-
-    chi_cur=chifun(data,pars)
+    tau = 0.0544
+    chi_cur=chifun(data,np.insert(pars,3,tau))
 
     #generate the weird projection space
     L=np.linalg.cholesky(corr_mat)
 
     for _ in range(nstep):
         pars_trial=pars+L@np.random.randn(npar)
-        if pars_trial[3] < 0:
-            pars_trial[3] = pars[3]
-        chi_trial=chifun(data,pars_trial)
+        chi_trial=chifun(data,np.insert(pars_trial,3,tau))
         #we now have chi^2 at our current location
         #and chi^2 in our trial location. decide if we take the step
         accept_prob=np.exp(-0.5*(chi_trial-chi_cur))
@@ -35,31 +42,25 @@ def run_mcmc(pars,data,corr_mat, chifun = ct.my_chi, nstep=500, time_out = None)
             pass
         elif time.time() - start_time > time_out:
             break
-    return np.array(chain),np.array(chivec)
+    return np.delete(np.array(chain),3,1) ,np.array(chivec)
 
 
 
 curvature = np.load("output/newton_curvature.npy")
 pars = np.load("output/newton_params.npy")
-temp_mat = np.zeros((len(pars), len(pars)))
-temp_mat[:3,:3] = curvature[:3,:3]
-temp_mat[3,3] = pars[3] * 0.01
-temp_mat[4:,4:] = curvature[3:,3:]
-temp_mat[4:,:3] = curvature[3:,:3]
-temp_mat[:3,4:] = curvature[:3,3:]
-curvature = temp_mat
+pars = np.delete(pars,3)
 wmap=ct.read_wmap()
 data=wmap[:,0:3]
 chain,chivec=run_mcmc(pars,data,curvature,nstep=500, time_out=60*3)
 
 plt.clf()
 plt.plot(chivec)
-plt.savefig("output/chivec1.png")
+plt.savefig("output/chivec1_t.png")
 plt.clf()
 plt.plot(chain[:,0])
-plt.savefig("output/chain1_0.png")
-np.save("output/chain1", chain)
-np.save("output/chivec1", chivec)
+plt.savefig("output/chain1_0_t.png")
+np.save("output/chain1_t", chain)
+np.save("output/chivec1_t", chivec)
 
 ##this def did not converge just yet so lets do it again with the prior that we have:
 delt=chain.copy()
@@ -72,13 +73,16 @@ mycov=delt.T@delt/chain.shape[0]
 chain2,chivec2=run_mcmc(pars,data,mycov,nstep=1000, time_out=60*10)
 plt.clf()
 plt.plot(chivec2)
-plt.savefig("output/chivec2.png")
+plt.savefig("output/chivec2_t.png")
 plt.clf()
 plt.plot(chain2[:,0])
-plt.savefig("output/chain2_0.png")
-np.save("output/chain2", chain2)
-np.save("output/chivec2", chivec2)
+plt.savefig("output/chain2_0_t.png")
+np.save("output/chain2_t", chain2)
+np.save("output/chivec2_t", chivec2)
+
+print("from new chain:")
+print("Best params are now: ", np.insert(np.mean(chain2, axis = 0),4, 0.0544))
+print("with errors given by: ", np.insert(np.std(chain2, axis = 0),4,0.0073))
 
 
-print("Best params are now: ", np.mean(chain2, axis = 0))
-print("with errors given by: ", np.std(chain2, axis = 0))
+
